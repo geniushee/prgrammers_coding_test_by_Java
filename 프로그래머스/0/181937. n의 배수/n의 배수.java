3816654bc4c1c40d64061b4ccfc4ff29db39cdf1
geniushee <129508219+geniushee@users.noreply.github.com>
2024-02-10 23:57:17 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        int mod = num % n;
+        if (mod == 0){
+            return 1;
+        }
+        return answer;
+    }
+}
