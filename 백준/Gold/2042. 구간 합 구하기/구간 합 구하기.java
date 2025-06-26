e9914c94be11f1bc223bec087f1d4934168c69ca
geniushee <129508219+geniushee@users.noreply.github.com>
2025-06-26 23:12:46 +0900

    [Gold I] Title: 구간 합 구하기, Time: 704 ms, Memory: 120160 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
-public class Main{
+class Main{
+    static long[] arr, tree;
     public static void main(String[] arg) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -8,39 +9,53 @@ public class Main{
         int n = Integer.parseInt(line[0]);
         int m = Integer.parseInt(line[1]);
         int k = Integer.parseInt(line[2]);
-        long[] arr = new long[n+1];
+        arr = new long[n+1];
+        tree = new long[4*n];
         for(int i = 1; i < n+1; i++){
             arr[i] = Long.parseLong(br.readLine());
         }
 
-        String flag;
+        buildTree(1,n,1);
 
-        for(int i = 0; i<m+k;i++){
+        for(int i = 0; i < m+k;i++){
             line = br.readLine().split(" ");
-            flag = line[0];
-            switch(flag){
-                case "1" -> change(arr, line[1], line[2]);
-                case "2" ->{
-                    long sum = partSum(arr, line[1], line[2]);
-                    bw.write(sum + "\n");
-                    bw.flush();
-                }
+            int a = Integer.parseInt(line[0]);
+            int b = Integer.parseInt(line[1]);
+            long c = Long.parseLong(line[2]);
+
+            if(a == 1){
+                update(1,n,1,b,c);
+            }else{
+                long sum = query(1,n, 1,b, (int) c);
+                bw.write(sum + "\n");
+                bw.flush();
             }
         }
         bw.close();
     }
-    private static long partSum(long[] arr, String s, String e) {
-        int start = Integer.parseInt(s);
-        int end = Integer.parseInt(e);
-        long sum = 0;
-        for(int i = start; i<=end; i++){
-            sum += arr[i];
-        }
-        return sum;
+    private static long buildTree(int start, int end, int node) {
+        if(start == end) return tree[node] = arr[start];
+        int mid = (start + end) / 2;
+        long left = buildTree(start, mid, node * 2);
+        long right = buildTree(mid + 1, end, node * 2 + 1);
+        return tree[node] = left + right;
     }
 
-    private static void change(long[] arr, String preIdx, String changed) {
-        int idx = Integer.parseInt(preIdx);
-        arr[idx] = Long.parseLong(changed);
+    private static long query(int start, int end, int node, int b, int c) {
+        if(b <= start && c >= end) return tree[node];
+        if(end < b || c < start) return 0;
+        int mid = (start + end) / 2;
+        long left = query(start, mid, node * 2, b, c);
+        long right = query(mid + 1, end, node * 2 +1, b,c);
+        return left + right;
+    }
+
+    private static long update(int start, int end,int node, int b, long c) {
+        if(b < start || b > end) return tree[node];
+        if(start == end) return tree[node] = c;
+        int mid = (start + end) / 2;
+        long left = update(start, mid, node * 2, b,c);
+        long right = update(mid + 1, end, node * 2 + 1, b,c);
+        return tree[node] = left + right;
     }
 }
